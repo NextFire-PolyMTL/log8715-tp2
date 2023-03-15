261e2a5bbe7be6747b5587f31f291aa134488302
matt <bassetmatt@gmail.com>
2023-03-15 17:08:56 -0400

    Modify prefabs and rename scripts

---
+++ Assets/Ex4/Scripts/Entity.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EntityType {
+    ETT_NONE = -1,
+    ETT_PLNT,
+    ETT_PREY,
+    ETT_PRED,
+}
+public class Entity : MonoBehaviour
+{
+    [SerializeField] EntityType type;
+    private Lifetime _lifetime;
+
+    void Awake()
+    {
+        _lifetime = GetComponent<Lifetime>();
+    }
+
+    void Start(){}
+
+    void Update()
+    {
+        if (type == EntityType.ETT_PLNT)
+            transform.localScale = Vector3.one * _lifetime.GetProgression();
+    }
+}
