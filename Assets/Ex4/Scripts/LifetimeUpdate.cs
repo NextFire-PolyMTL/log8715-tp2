c492ff7445160829ad77fcb611744e850184e7c3
matt <bassetmatt@gmail.com>
2023-03-18 18:39:39 -0400

    Documentation, final refactoring

--- Assets/Ex4/Scripts/LifetimeUpdate.cs
+++ Assets/Ex4/Scripts/LifetimeUpdate.cs
@@ -1,52 +1,107 @@
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
-using static JobHandler;
 
-public class LifeTimeUpdate
-{
-    public static NativeArray<Vector3> emptyArray = new NativeArray<Vector3>(0, Allocator.Persistent);
+/* Defines the Job Structure for Lifetime management */
+public class LifeTimeUpdate {
 
-    public static void ApplyJob()
-    {
-        int plantCount = SimulationMain.PlantPos.Length;
-        int preyCount = SimulationMain.PreyPos.Length;
-        int predCount = SimulationMain.PredPos.Length;
-        var emptyArray =  new NativeArray<Vector3>(0, Allocator.TempJob);
+	/* Job Handling the update of all lifetime entities */
+	[BurstCompile(CompileSynchronously = true)]
+	public struct LifeTimeJob : IJobParallelFor {
+		/* Iterated on. The entity position */
+		[ReadOnly] public NativeArray<Vector3> ownPos;
+		/* Positions of the entities that can accelerate the dying process */
+		[ReadOnly] public NativeArray<Vector3> accPos;
+		/* Positions of the entities that can slow the dying process down*/
+		[ReadOnly] public NativeArray<Vector3> slowPos;
+		/* Positions of the of the same type for reproduction purposes */
+		[ReadOnly] public NativeArray<Vector3> ownTypePos;
+		/* Iterated on and modified. The parameters of the `Lifetime` class */
+		public NativeArray<LTParams> paramArray;
+		/* The touching distance defined on the config*/
+		[ReadOnly] public float touchDist;
 
-        var jobPlant = new LifeTimeJob() {
-            ownPos = SimulationMain.PlantPos,
-            accPos = SimulationMain.PreyPos,
-            slowPos = emptyArray,
-            ownTypePos = emptyArray,
-            paramArray = SimulationMain.PlantLTParams,
-            touchDist = Ex4Config.TouchingDistance
-        };
-        var jobPrey = new LifeTimeJob() {
-            ownPos = SimulationMain.PreyPos,
-            accPos = SimulationMain.PredPos,
-            slowPos = SimulationMain.PlantPos,
-            ownTypePos = SimulationMain.PredPos,
-            paramArray = SimulationMain.PreyLTParams,
-            touchDist = Ex4Config.TouchingDistance
-        };
-        var jobPred = new LifeTimeJob() {
-            ownPos = SimulationMain.PredPos,
-            accPos = emptyArray,
-            slowPos = SimulationMain.PreyPos,
-            ownTypePos = SimulationMain.PredPos,
-            paramArray = SimulationMain.PredLTParams,
-            touchDist = Ex4Config.TouchingDistance
-        };
+		public void Execute(int i) {
+			var newParams = paramArray[i];
+			newParams.decrFactor = 1.0f;
+			/* Checking contact with enemies*/
+			foreach (var pos in accPos) {
+				if (Vector3.Distance(pos, ownPos[i]) < touchDist) {
+					newParams.decrFactor *= 2f;
+					break;
+				}
+			}
+			/* Checking contact with food */
+			foreach (var pos in slowPos) {
+				if (Vector3.Distance(pos, ownPos[i]) < touchDist) {
+					newParams.decrFactor /= 2f;
+					break;
+				}
+			}
+			/* Skips check if the reproduce flag is already activated since
+				* it cannot be toggled off */
+			if (newParams.reproduceFlag) {
+				paramArray[i] = newParams;
+				return;
+			}
+			/* Checking reproduce flag with own species while being sure the
+				* entity doesn't reproduce with itself */
+			foreach(var pos in ownTypePos) {
+				float dist = Vector3.Distance(pos, ownPos[i]);
+				if (!Mathf.Approximately(dist, 0f) && dist < touchDist) {
+					newParams.reproduceFlag = true;
+					break;
+				}
+			}
+			paramArray[i] = newParams;
+		}
+	}
 
-        var JobHandlerPlant = jobPlant.Schedule<LifeTimeJob>(plantCount, 64);
-        var JobHandlerPrey  = jobPrey.Schedule< LifeTimeJob>(preyCount , 64);
-        var JobHandlerPred  = jobPred.Schedule< LifeTimeJob>(predCount , 64);
 
-        JobHandlerPlant.Complete();
-        JobHandlerPrey.Complete();
-        JobHandlerPred.Complete();
+	public static void ApplyJob() {
+		int plantCount = SimulationMain.PlantPos.Length;
+		int preyCount = SimulationMain.PreyPos.Length;
+		int predCount = SimulationMain.PredPos.Length;
+		/* Special variable used for entity types that e.g. don't have another
+		 * entity that can slow down their dying process. So this array skips
+		 * the loop while being an acceptable parameter for the job */
+		var emptyArray =  new NativeArray<Vector3>(0, Allocator.TempJob);
 
-        emptyArray.Dispose();
-    }
+		var jobPlant = new LifeTimeJob() {
+			ownPos = SimulationMain.PlantPos,
+			accPos = SimulationMain.PreyPos,
+			slowPos = emptyArray,
+			ownTypePos = emptyArray,
+			paramArray = SimulationMain.PlantLTParams,
+			touchDist = Ex4Config.TouchingDistance
+		};
+		var jobPrey = new LifeTimeJob() {
+			ownPos = SimulationMain.PreyPos,
+			accPos = SimulationMain.PredPos,
+			slowPos = SimulationMain.PlantPos,
+			ownTypePos = SimulationMain.PredPos,
+			paramArray = SimulationMain.PreyLTParams,
+			touchDist = Ex4Config.TouchingDistance
+		};
+		var jobPred = new LifeTimeJob() {
+			ownPos = SimulationMain.PredPos,
+			accPos = emptyArray,
+			slowPos = SimulationMain.PreyPos,
+			ownTypePos = SimulationMain.PredPos,
+			paramArray = SimulationMain.PredLTParams,
+			touchDist = Ex4Config.TouchingDistance
+		};
+
+		var JobHandlerPlant = jobPlant.Schedule<LifeTimeJob>(plantCount, 64);
+		var JobHandlerPrey  = jobPrey.Schedule< LifeTimeJob>(preyCount , 64);
+		var JobHandlerPred  = jobPred.Schedule< LifeTimeJob>(predCount , 64);
+
+		JobHandlerPlant.Complete();
+		JobHandlerPrey.Complete();
+		JobHandlerPred.Complete();
+
+		/* Avoids memory leaks with this array */
+		emptyArray.Dispose();
+	}
 }
--- Assets/Ex4/Scripts/PositionUpdate.cs
+++ Assets/Ex4/Scripts/PositionUpdate.cs
@@ -1,32 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
-using static JobHandler;
 
-public class PositionUpdate
-{
-    public static void ApplyJob()
-    {
-        int preyCount = SimulationMain.PreyPos.Length;
-        int predCount = SimulationMain.PredPos.Length;
+/* Defines the Job Structure for Position update from velocity */
+public class PositionUpdate {
+	/* Job handlign the position update */
+	[BurstCompile(CompileSynchronously = true)]
+	public struct PositionJob : IJobParallelFor {
+		/* Iterated on. The entity's velocity */
+		[ReadOnly] public NativeArray<Vector3> velocity;
+		/* Iterated on and modified. The entity's position */
+		public NativeArray<Vector3> position;
+		/* `Time.deltaTime` passed as an argument */
+		[ReadOnly] public float deltaTime;
 
-        var preyJob = new PositionJob() {
-            velocity = SimulationMain.PreyVel,
-            position = SimulationMain.PreyPos,
-            deltaTime = Time.deltaTime
-        };
+		public void Execute(int i) {
+			position[i] += velocity[i] * deltaTime;
+		}
+	}
 
-        var predJob = new PositionJob() {
-            velocity = SimulationMain.PredVel,
-            position = SimulationMain.PredPos,
-            deltaTime = Time.deltaTime
-        };
+	public static void ApplyJob() {
+		int preyCount = SimulationMain.PreyPos.Length;
+		int predCount = SimulationMain.PredPos.Length;
 
-        JobHandle preyJH = preyJob.Schedule(preyCount, 64);
-        JobHandle predJH = predJob.Schedule(predCount, 64);
+		var preyJob = new PositionJob() {
+			velocity = SimulationMain.PreyVel,
+			position = SimulationMain.PreyPos,
+			deltaTime = Time.deltaTime
+		};
 
-        preyJH.Complete();
-        predJH.Complete();
-    }
+		var predJob = new PositionJob() {
+			velocity = SimulationMain.PredVel,
+			position = SimulationMain.PredPos,
+			deltaTime = Time.deltaTime
+		};
+
+		JobHandle preyJH = preyJob.Schedule(preyCount, 64);
+		JobHandle predJH = predJob.Schedule(predCount, 64);
+
+		preyJH.Complete();
+		predJH.Complete();
+	}
 }
